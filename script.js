62f2b1189d6e33ba75ded4d02d526bd70541b72e
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-03-14 17:50:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+let statusElement = document.getElementById('status');
+let enterButton = document.getElementById('enterBtn');
+let status = "Enter the Metaverse";
+statusElement.innerHTML = status;
+enterButton.addEventListener("click",()=>{
+	status = "Entered Metaverse";
+	statusElement.innerHTML = status;
+});
